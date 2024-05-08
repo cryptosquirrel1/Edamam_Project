0f728e792db5a6b6f7572d88d3d610bf1b9904e1
cryptosquirrel1 <91976593+cryptosquirrel1@users.noreply.github.com>
2024-05-08 14:01:54 +0200

    Update 02 Final Project - Code from Student Guide_Simplified.py

--- 02 Final Project - Code from Student Guide_Simplified.py
+++ 02 Final Project - Code from Student Guide_Simplified.py
@@ -1,1 +1,18 @@
+"""
+============================================================================
+SIMPLIFYING STUDENT CODE GUIDE
+============================================================================
+"""
 
+import recipe
+import requests
+
+recipe_search = input('enter a ingredient: ')
+url = 'https://api.edamam.com/api/recipes/v2?type=public&beta=true&q={}&app_id=5ec8b15f&app_key=f872fab13b04a660a923e43c2e9f5654&field=uri&field=label'.format(recipe_search)
+
+result = requests.get(url)
+data = result.json()
+retun data['hits']
+
+print(data['hits']['recipe'])        
+#only works when i add an integer in between. Why is that?. Error List indices must be integers or slices, not str. my objective is to return the sub-value of 'hits'
