fe625ec70419a82e33c74109c60f131dd49952af
cryptosquirrel1 <91976593+cryptosquirrel1@users.noreply.github.com>
2024-05-08 13:58:46 +0200

    Update 02 Final Project - Code from Student Guide.py

--- 02 Final Project - Code from Student Guide.py
+++ 02 Final Project - Code from Student Guide.py
@@ -10,7 +10,7 @@ CODE FROM STUDENT GUIDE
 # Register to get APP ID and KEY https://developer.edamam.com/
 #     app_id = '5ec8b15f'
 #     app_key = 'f872fab13b04a660a923e43c2e9f5654'
-#     result = requests.get(f'https://api.edamam.com/api/recipes/v2?type=public&beta=true&q={recipe_search}&app_id={app_id}&app_key={app_key}').format(ingredient,app_id,app_key)
+#     result = requests.get(f'https://api.edamam.com/api/recipes/v2?type=public&beta=true&q={ingredient}&app_id={app_id}&app_key={app_key}').format(ingredient,app_id,app_key)
 #     data = result.json()
 #     return data['hits']                        ### RETURN DATA SEEMS TO BE AN ERROR
 #
